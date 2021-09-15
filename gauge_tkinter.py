7fe5b4eab0f13f68956adea50b8c51af78f91b89
kri628 <00a3d2@duksung.ac.kr>
2021-09-16 01:01:55 +0900

    필요없는 부분 삭제, 레이블 추가

--- gauge_tkinter.py
+++ gauge_tkinter.py
@@ -1,60 +1,22 @@
+import time
 import threading
 import tkinter as tk
 from tkinter import ttk
-import random
-from PIL import Image, ImageTk, ImageDraw, ImageFont
-from sklearn.preprocessing import MinMaxScaler
-import collections
-import time
 
-from serial import Serial
-from tensorflow.keras.models import load_model
+from PIL import Image, ImageTk, ImageDraw
 
-# model = load_model('lstm_class.h5')
-# ser = Serial('/dev/ttyACM0', 115200)
-# p = collections.deque()
-# SIZE = 100
-
-WIN_SIZE = (500, 600)
+SIZE = (500, 600)
 ARC_SIZE = 100
+
 angle = -180
+
 
 def get_degree():
     global angle
     while True:
-        '''
-        if ser.readable():
-            res = ser.readline()
-            s = res.decode()[:len(res) - 1]
+        angle += 10
+        time.sleep(1)
 
-            a = s.split(',')
-            try:
-                # parsing
-                pa0 = int(a[3].split()[-1])
-                pa1 = int(a[4].split()[-1])
-                # print(pa0, pa1)
-
-            except IndexError:
-                continue
-
-            if len(p) < SIZE:
-                p.append([pa0, pa1])
-                continue
-            else:
-                p.popleft()
-                p.append([pa0, pa1])
-            # MinMaxScale (0~1)
-            scaler = MinMaxScaler()
-            data = scaler.fit_transform(p)
-            data = np.reshape(data, (1, SIZE, 2))
-
-            pred = np.argmax(model.predict(data), axis=-1)
-            angle = list(map(lambda x: x * 10 - 90, pred))
-            print(pred, angle, a[0].split()[-1])
-            '''
-        angle =random.randrange(-90,90,10)
-        angle-=90
-        time.sleep(0.2)
 
 class Gauge(ttk.Label):
 
@@ -95,28 +57,30 @@ class Gauge(ttk.Label):
             self.im = Image.new('RGBA', (1000, 1000))
             draw = ImageDraw.Draw(self.im)
             draw.arc((0, 0, 990, 990), -180, 0, self.troughcolor, ARC_SIZE)
-            draw.arc((0, 0, 990, 990), angle-1, angle+1, self.indicatorcolor, ARC_SIZE)
-
-            font = ImageFont.truetype(font="AppleGothic.ttf", size=50)
-            draw.text((450, 500), str(angle+90), (0,0,0), font)
-
+            draw.arc((0, 0, 990, 990), -180, angle, self.indicatorcolor, ARC_SIZE)
             self.arc = ImageTk.PhotoImage(self.im.resize((self.size, self.size), Image.LANCZOS))
             self.configure(image=self.arc)
-
-            time.sleep(0.2)
+            text.set(angle)
+            label.configure()
+            time.sleep(1)
 
 
 if __name__ == '__main__':
     root = tk.Tk()
-    root.geometry(str(WIN_SIZE[0]) + 'x' + str(WIN_SIZE[1]))
-    root.title("Angle")
-
+    root.geometry(str(SIZE[0]) + 'x' + str(SIZE[1]))
     style = ttk.Style()
     gauge = Gauge(root, padding=50)
     gauge.pack()
     # ttk.Scale(root, from_=0, to=180, variable=gauge.arcvariable).pack(fill='x', padx=10, pady=10)
     # update the textvariable with the degrees information when the arcvariable changes
     # gauge.arcvariable.trace_add('write', lambda *args, g=gauge: g.textvariable.set(f'{g.arcvariable.get()} deg'))
+
+    text = tk.StringVar()
+    text.set(angle)
+    label = tk.Label(root,
+                     textvariable=text,
+                     font=("gothic", "20"))
+    label.pack()
 
     t1 = threading.Thread(target=get_degree)
     t1.start()
